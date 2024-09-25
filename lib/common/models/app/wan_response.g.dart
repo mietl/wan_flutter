6bd6bbcd8dc61a9b789e15829b2499ca7a31728b
mietl <mietl.moe@icloud.com>
2024-09-26 00:10:45 +0800

    dart -fix

--- lib/common/models/app/wan_response.g.dart
+++ lib/common/models/app/wan_response.g.dart
@@ -11,7 +11,7 @@ ObjectResponse<T> _$ObjectResponseFromJson<T>(
   T Function(Object? json) fromJsonT,
 ) =>
     ObjectResponse<T>(
-      data: _$nullableGenericFromJson(json['data'], fromJsonT),
+      data: fromJsonT(json['data']),
       errorCode: (json['errorCode'] as num).toInt(),
       errorMsg: json['errorMsg'] as String,
     );
@@ -21,29 +21,17 @@ Map<String, dynamic> _$ObjectResponseToJson<T>(
   Object? Function(T value) toJsonT,
 ) =>
     <String, dynamic>{
-      'data': _$nullableGenericToJson(instance.data, toJsonT),
+      'data': toJsonT(instance.data),
       'errorCode': instance.errorCode,
       'errorMsg': instance.errorMsg,
     };
-
-T? _$nullableGenericFromJson<T>(
-  Object? input,
-  T Function(Object? json) fromJson,
-) =>
-    input == null ? null : fromJson(input);
-
-Object? _$nullableGenericToJson<T>(
-  T? input,
-  Object? Function(T value) toJson,
-) =>
-    input == null ? null : toJson(input);
 
 ListResponse<T> _$ListResponseFromJson<T>(
   Map<String, dynamic> json,
   T Function(Object? json) fromJsonT,
 ) =>
     ListResponse<T>(
-      data: (json['data'] as List<dynamic>?)?.map(fromJsonT).toList(),
+      data: (json['data'] as List<dynamic>).map(fromJsonT).toList(),
       errorCode: (json['errorCode'] as num).toInt(),
       errorMsg: json['errorMsg'] as String,
     );
@@ -53,7 +41,7 @@ Map<String, dynamic> _$ListResponseToJson<T>(
   Object? Function(T value) toJsonT,
 ) =>
     <String, dynamic>{
-      'data': instance.data?.map(toJsonT).toList(),
+      'data': instance.data.map(toJsonT).toList(),
       'errorCode': instance.errorCode,
       'errorMsg': instance.errorMsg,
     };
--- lib/pages/login/index.dart
+++ lib/pages/login/index.dart
@@ -9,7 +9,8 @@ import 'package:wan_flutter/common/utils/shared_preferences_util.dart';
 import 'package:wan_flutter/common/utils/toast.dart';
 import 'package:wan_flutter/common/utils/show_dialog.dart';
 import 'package:wan_flutter/pages/home/index.dart';
-class LoginPage extends StatefulWidget{
+
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
@@ -18,11 +19,11 @@ class LoginPage extends StatefulWidget{
   }
 }
 
-
-
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController usernameController = TextEditingController(text: 'mietl');
-  TextEditingController passwordController = TextEditingController(text: 't22140127L');
+  TextEditingController usernameController =
+      TextEditingController(text: 'mietl');
+  TextEditingController passwordController =
+      TextEditingController(text: 't22140127L');
 
   //边框样式
   final OutlineInputBorder _outlineInputBorder = const OutlineInputBorder(
@@ -32,9 +33,8 @@ class _LoginPageState extends State<LoginPage> {
     ),
   );
 
-
-  InputDecoration getInputDecoration(String hintText){
-      return InputDecoration(
+  InputDecoration getInputDecoration(String hintText) {
+    return InputDecoration(
         filled: true,
         fillColor: Colors.white,
         enabledBorder: _outlineInputBorder,
@@ -43,51 +43,51 @@ class _LoginPageState extends State<LoginPage> {
         errorBorder: _outlineInputBorder,
         isDense: true,
         hintText: hintText,
-        floatingLabelBehavior: FloatingLabelBehavior.never
-    );
+        floatingLabelBehavior: FloatingLabelBehavior.never);
   }
 
   _startLogin() async {
     var username = usernameController.text;
     var password = passwordController.text;
 
-    if(username.isEmpty){
+    if (username.isEmpty) {
       return toast('用户名不能为空');
     }
-    if(password.isEmpty){
+    if (password.isEmpty) {
       return toast('密码不能为空');
     }
 
-    showLoadingBox(context,canPop: false);
-    
-    try{
-       var response  = await UserApi.passwordLogin(username,password);
-       toast('登录成功!');
-       if(mounted){
-         Navigator.pop(context);
-       }
+    showLoadingBox(context, canPop: false);
 
-       List<String>? cookies = response.headers['Set-Cookie'];
-       if(cookies!=null){
-         WanHttpUtil.setCookies(cookies);
+    try {
+      var response = await UserApi.passwordLogin(username, password);
+      toast('登录成功!');
+      if (mounted) {
+        Navigator.pop(context);
+      }
 
-         var user  = ObjectResponse<UserInfo>.fromJson(response.data,(json)=>UserInfo.fromJson(json));
-         if(mounted){
-           Provider.of<UserProvider>(context,listen: false).setUserInfo(user.data);
-           SharedPreferencesUtil().setStringList('cookies', cookies);
-           // 首页
-           Navigator.push(context, MaterialPageRoute(
-             builder: (BuildContext context)=>const HomePage()
-           ));
-         }
-       }
-    }catch (e) {
-      if(e is WanException){
+      List<String>? cookies = response.headers['Set-Cookie'];
+      WanHttpUtil.setCookies(cookies);
+
+      var user = ObjectResponse<UserInfo>.fromJson(
+          response.data, (json) => UserInfo.fromJson(json));
+      if (mounted) {
+        Provider.of<UserProvider>(context, listen: false)
+            .setUserInfo(user.data);
+        SharedPreferencesUtil().setStringList('cookies', cookies ?? []);
+        // 首页
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => const HomePage()));
+      }
+    } catch (e) {
+      if (e is WanException) {
         toast('登录失败: ${e.message}');
-      }else{
+      } else {
         toast('登录失败: $e');
       }
-      if(mounted){
+      if (mounted) {
         Navigator.pop(context);
       }
     }
@@ -96,55 +96,51 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFf5f5f7),
-      appBar: AppBar(
-        // backgroundColor: const Color(0xFFe8dfd8),
-        title: const Text('登录'),
-        elevation: 4,
-      ),
-      body:  Padding(
-        padding: const EdgeInsets.all(14),
-        child: Column(
-          children: [
-            TextField(
-              autofocus: true,
-              decoration: getInputDecoration('用户名'),
-              controller: usernameController,
-            ),
-            const SizedBox( height: 14),
-            TextField(
-              decoration: getInputDecoration('密码'),
-              controller: passwordController,
-              obscureText: true,
-            ),
-            const SizedBox(height: 14),
-            MaterialButton(
-              disabledColor: const Color(0xFFfcf4e8),
-              color: const Color(0xFFeec8bd),
-              minWidth: double.infinity,
-              height: 44,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10)
-              ),
-              elevation: 0,
-              onPressed: _startLogin,
-              child: const Text('登录',style: TextStyle(fontSize: 18)),
-            ),
-            const SizedBox(height: 7.0),
-            Align(
-              alignment: Alignment.bottomRight,
-              child:InkWell(
-                onTap: (){
-
-                },
-                child: const Text('去注册',
-                    style: TextStyle(fontSize: 16,decoration: TextDecoration.underline)
-                ),
-              )
-            )
-          ],
+        backgroundColor: const Color(0xFFf5f5f7),
+        appBar: AppBar(
+          // backgroundColor: const Color(0xFFe8dfd8),
+          title: const Text('登录'),
+          elevation: 4,
         ),
-      )
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(14),
+          child: Column(
+            children: [
+              TextField(
+                autofocus: true,
+                decoration: getInputDecoration('用户名'),
+                controller: usernameController,
+              ),
+              const SizedBox(height: 14),
+              TextField(
+                decoration: getInputDecoration('密码'),
+                controller: passwordController,
+                obscureText: true,
+              ),
+              const SizedBox(height: 14),
+              MaterialButton(
+                disabledColor: const Color(0xFFfcf4e8),
+                color: const Color(0xFFeec8bd),
+                minWidth: double.infinity,
+                height: 44,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 0,
+                onPressed: _startLogin,
+                child: const Text('登录', style: TextStyle(fontSize: 18)),
+              ),
+              const SizedBox(height: 7.0),
+              Align(
+                  alignment: Alignment.bottomRight,
+                  child: InkWell(
+                    onTap: () {},
+                    child: const Text('去注册',
+                        style: TextStyle(
+                            fontSize: 16,
+                            decoration: TextDecoration.underline)),
+                  ))
+            ],
+          ),
+        ));
   }
 }
